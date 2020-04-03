e52e58c05b19e2703a5b18d366c22c744b29260b
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-03 19:53:13 -0400

    feat: replaced Links with NavLinks in the Dashboard component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Adapter from './Adapter';
+import { NavLink } from 'react-router-dom';
 
-import Navbar from './Navbar';
-import FormModal from './FormModal'
+import Adapter from './Adapter';
+import FormModal from './FormModal';
 
 const renderItem = post => (
   <li key={post.id}>
     <div className="card post-preview">
-      <Link to={'/post/' + post.id} className="card-link">
+      <NavLink to={'/posts/' + post.id} className="card-link">
         <div
           className="card text-white bg-primary mb-3 card-margin"
           styles={{ maxWidth: '20rem' }}
@@ -18,7 +17,7 @@ const renderItem = post => (
             <p className="card-text">CONTENT PLACEHOLDER</p>
           </div>
         </div>
-      </Link>
+      </NavLink>
     </div>
   </li>
 );
@@ -28,22 +27,21 @@ const renderListItems = items => items.map(renderItem);
 const Dashboard = () => {
   const [posts, setPosts] = useState([]);
 
-  const [requestFormOpen, setRequestFormOpen] = useState(false)
+  const [requestFormOpen, setRequestFormOpen] = useState(false);
 
   useEffect(() => {
     Adapter.getPosts(setPosts);
   }, []);
 
   const showModal = () => {
-    console.log('handled click')
-    setRequestFormOpen(true)
-  }
+    console.log('handled click');
+    setRequestFormOpen(true);
+  };
 
   const closeModal = () => {
-    console.log('closed form')
-    setRequestFormOpen(false)
-  }
-
+    console.log('closed form');
+    setRequestFormOpen(false);
+  };
 
   // # t.string :title
   // # t.boolean :request_offer
@@ -56,22 +54,24 @@ const Dashboard = () => {
 
   return (
     <div>
-      <Navbar />
-
       <div className="board">
-      <button type="button" 
-              className="btn btn-success btn-lg btn-block"
-              onClick={showModal}>
-              
-              Request Aid</button>
+        <button
+          type="button"
+          className="btn btn-success btn-lg btn-block"
+          onClick={showModal}
+        >
+          Request Aid
+        </button>
         <ul>{renderListItems(posts)}</ul>
 
-        <FormModal showModal={showModal} closeModal={closeModal} formOpen={requestFormOpen}>
+        <FormModal
+          showModal={showModal}
+          closeModal={closeModal}
+          formOpen={requestFormOpen}
+        >
           <p>Modal</p>
           <p>Data</p>
         </FormModal>
-        
-
       </div>
     </div>
   );
