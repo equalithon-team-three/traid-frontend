f6c90e2e28b449c5eb018d3cda5bf0670a0af9a1
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-03 19:53:59 -0400

    feat: replaced Switch and Routes with the Routes component and plugged the Navbar component in the App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Post from './Post';
+
+import Navbar from './Navbar';
+import Routes from './Routes';
 import './App.css';
 
-const renderPost = ({ match }) => console.log(match) || <Post id={ match.params.id } />
-
 const App = () => {
+  return (
+    <div>
+      <Navbar />
 
-  return (<Switch>
-    <Route path="/" exact component={ Dashboard } />
-    <Route path="/post/:id" render={ renderPost } />
-  </Switch>
-
+      <Routes />
+    </div>
   );
-}
+};
 
 export default App;
