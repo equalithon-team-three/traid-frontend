d1fa47918b757338c1b8806a180803a5c92aa9ab
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2020-04-02 21:35:10 -0400

    .env and dependencies

--- src/Post.js
+++ src/Post.js
@@ -3,11 +3,11 @@ import Adapter from './Adapter';
 
 const Post = ({ id: postId }) => {
 
-  const [ post, setPost ] = useState([])
+  const [ post, setPost ] = useState([]);
 
   useEffect(() => {
     Adapter.getPost(postId, setPost)
-  }, [])
+  }, [ postId, setPost ]); // no array runs on every render, empty array is essentially componentDidMount
 
   return <>
     <h1>{ post.title }</h1>
