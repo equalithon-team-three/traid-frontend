c6bc189ebb3acba607c2fe1051b715f29596ff8a
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2020-04-04 22:26:53 -0400

    details about a post

--- src/Post.js
+++ src/Post.js
@@ -3,7 +3,7 @@ import Adapter from './Adapter';
 import PostCard from './PostCard';
 
 const Post = ({ id: postId }) => {
-  const [post, setPost] = useState({matching_posts: []});
+  const [post, setPost] = useState({ matching_posts: [], post_category: {}, user: {} });
 
   useEffect(() => {
     Adapter.getPost(postId, setPost);
@@ -11,10 +11,16 @@ const Post = ({ id: postId }) => {
 
   return (
     <>
-      <h1>{post.title}</h1>
+      <h1>{ post.request_offer ? "Request" : "Offer" }: {post.title}</h1>
+      <p>From: { post.user.first_name + " " + post.user.last_name }</p>
+      <p>{post.post_category.name}</p>
+      <p>Location: {post.location}</p>
+      <p>Deadline: {post.deadline}</p>
+      <p>Email: <a href={ "mailto:" + post.user.email }>{ post.user.email }</a></p>
+      <p>Quantity: {post.quantity}</p>
       <p class="lead">{post.details}</p>
 
-      <p class='lead'>Related Posts</p>
+      <h2>{ post.request_offer ?  "Requests" : "Offers" } in this category</h2>
       <ul>
         {post.matching_posts.map(post => {
           return <PostCard post={post}/>
