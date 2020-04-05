51702ee9539a7e525aec4effd3fa96837505a953
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-04 22:50:10 -0400

    feat: passed authData to the UserPosts component through the /myposts route in the Routes component

--- src/Routes.js
+++ src/Routes.js
@@ -6,30 +6,42 @@ import Dashboard from './Dashboard';
 import Post from './Post';
 import Auth from './Auth';
 import LogOut from './LogOut';
+import UserPosts from './UserPosts';
 import PageNotFound from './PageNotFound';
 
+// Initializations
 const renderPost = ({ match }) => <Post id={match.params.id} />;
-
-const renderAuth = (setAuthenticated, loggingIn) => renderProps => <Auth { ...renderProps } 
-  loggingIn={ loggingIn} 
-  setAuthenticated= { setAuthenticated } />
+const renderAuth = (setAuthenticated, loggingIn) => renderProps => (
+  <Auth
+    {...renderProps}
+    loggingIn={loggingIn}
+    setAuthenticated={setAuthenticated}
+  />
+);
 
 // Component
-const Routes = ({ setAuthenticated: setAuthenticatedProp, history }) => {
-
+const Routes = ({
+  authData,
+  authsetAuthenticated: setAuthenticatedProp,
+  history,
+}) => {
   const setAuthenticated = data => {
-    history.push("/")
-    setAuthenticatedProp(data)
-  }
+    history.push('/');
+    setAuthenticatedProp(data);
+  };
 
   return (
     <Switch>
       <Route path="/" exact component={Dashboard} />
       <Route path="/posts/:id" render={renderPost} />
-      <Route path="/login" render={ renderAuth(setAuthenticated, true) } />
-      <Route path="/register" render={ renderAuth(setAuthenticated) } />
-      <Route path="/logout" render={ () => <LogOut setAuthenticated={ setAuthenticated } /> } />
-      <Route path="/:wildcard" component={ PageNotFound } />
+      <Route path="/myposts" render={() => <UserPosts authData={authData} />} />
+      <Route path="/login" render={renderAuth(setAuthenticated, true)} />
+      <Route path="/register" render={renderAuth(setAuthenticated)} />
+      <Route
+        path="/logout"
+        render={() => <LogOut setAuthenticated={setAuthenticated} />}
+      />
+      <Route path="/:wildcard" component={PageNotFound} />
     </Switch>
   );
 };
