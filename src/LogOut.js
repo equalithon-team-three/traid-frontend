16e1549e040ae62dce63e2fbc8bf47608542d49a
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-04 22:47:15 -0400

    chore: added setAuthenticated as a useEffect dependency in the LogOut component

--- src/LogOut.js
+++ src/LogOut.js
@@ -2,14 +2,11 @@ import React, { useEffect } from 'react';
 import Adapter from './Adapter';
 
 const LogOut = ({ setAuthenticated }) => {
-
   useEffect(() => {
-    Adapter.logOut(setAuthenticated)
-  }, [])
+    Adapter.logOut(setAuthenticated);
+  }, [setAuthenticated]);
 
-  return <span>Logging out...</span>
-}
-
-
+  return <span>Logging out...</span>;
+};
 
 export default LogOut;
