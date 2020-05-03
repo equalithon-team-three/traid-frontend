11c2663827bad1f49b8dbf86eea975782ff49353
Tal Luigi <luigi.t.agr@gmail.com>
2020-05-03 18:58:28 -0400

    feat: used moment to format post time stamp, applied key properties to child components that were being mapped in the Dashboard and Post components, as well as applied css classes to improve layout and styling

--- src/Post.js
+++ src/Post.js
@@ -1,31 +1,56 @@
 import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+
 import Adapter from './Adapter';
 import PostCard from './PostCard';
 
 const Post = ({ id: postId }) => {
-  const [post, setPost] = useState({ matching_posts: [], post_category: {}, user: {} });
+  const [post, setPost] = useState({
+    matching_posts: [],
+    post_category: {},
+    user: {},
+  });
 
   useEffect(() => {
     Adapter.getPost(postId, setPost);
-  }, [postId, setPost]); // no array runs on every render, empty array is essentially componentDidMount
+  }, [postId, setPost]); // No array runs on every render, empty array is essentially componentDidMount
 
   return (
-    <>
-      <h1>{ post.request_offer ? "Request" : "Offer" }: {post.title}</h1>
-      <p>{post.post_category.name}</p>
-      <p>Location: {post.location}</p>
-      <p>Deadline: {post.deadline}</p>
-      <p>Email: <a href={ "mailto:" + post.user.email }>{ post.user.email }</a></p>
-      <p>Quantity: {post.quantity}</p>
-      <p class="lead">{post.details}</p>
+    <div className="text-center board">
+      <h1>
+        {post.request_offer ? 'Request' : 'Offer'}: {post.title}
+      </h1>
 
-      <h2>{ post.request_offer ? "Offers" : "Requests" } in this category</h2>
+      <p>{`Category: ${post.post_category.name}`}</p>
+
+      <p>{`Location: ${post.location ? post.location : 'N/A'}`}</p>
+
+      <p>
+        {`Deadline: ${
+          post.deadline ? moment(post.deadline).format('L') : 'N/A'
+        }`}
+      </p>
+
+      <p>
+        Email: <a href={'mailto:' + post.user.email}>{post.user.email}</a>
+      </p>
+
+      <p>{`Quantity: ${post.quantity ? post.quantity : 'N/A'}`}</p>
+
+      <h5>Description:</h5>
+
+      <p className="text-color-gray">{`${post.details}`}</p>
+
+      <h2>
+        {`${post.request_offer ? 'Offers' : 'Requests'} In This Category`}
+      </h2>
+
       <ul>
-        {post.matching_posts.map(post => {
-          return <PostCard post={post}/>
-        })}
+        {post.matching_posts.map(curPost => (
+          <PostCard key={curPost.id} post={curPost} />
+        ))}
       </ul>
-    </>
+    </div>
   );
 };
 
